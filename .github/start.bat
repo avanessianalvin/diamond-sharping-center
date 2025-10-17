bc4952bb9ba466793dcff9a3951082807808c3c9
alvin <avanessian.alvin@gmail.com>
2025-10-17 17:34:48 +0400

    frontend build in backend

--- .github/start.bat
+++ .github/start.bat
@@ -3,7 +3,7 @@ cd /d C:\root\hama\backend\target
 
 for %%f in (*.jar) do (
     echo Starting %%f ...
-    start "" cmd /c "java -jar \"%%f\" --server.port=8090"
+    start "" java -jar "%%f" --server.port=8090
 )
 
 exit /b
